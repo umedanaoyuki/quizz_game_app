1eacf509022daac689a54218f8dfb879123f9dfc
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-07-14 17:46:57 +0900

    正解・不正解判定の処理をまとめる

--- app.js
+++ app.js
@@ -27,36 +27,27 @@ document.getElementById("js-question").textContent = question;
 //   document.getElementsByTagName("button")[i].textContent = answers[i];
 // }
 
-// ボタンをクリックしたら正誤判定
-$button[0].addEventListener("click", (e) => {
-  // console.log(e);
+const clickHandler = (e) => {
   if (correct === e.target.textContent) {
     window.alert("正解!");
   } else {
     window.alert("不正解!");
   }
+};
+
+// ボタンをクリックしたら正誤判定
+$button[0].addEventListener("click", (e) => {
+  clickHandler(e);
 });
 
 $button[1].addEventListener("click", (e) => {
-  if (correct === e.target.textContent) {
-    window.alert("正解!");
-  } else {
-    window.alert("不正解!");
-  }
+  clickHandler(e);
 });
 
 $button[2].addEventListener("click", (e) => {
-  if (correct === e.target.textContent) {
-    window.alert("正解!");
-  } else {
-    window.alert("不正解!");
-  }
+  clickHandler(e);
 });
 
 $button[3].addEventListener("click", (e) => {
-  if (correct === e.target.textContent) {
-    window.alert("正解!");
-  } else {
-    window.alert("不正解!");
-  }
+  clickHandler(e);
 });

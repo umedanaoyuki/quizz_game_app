d7e27b67a94802552a19b86cdc2ec31565f9dd58
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-07-14 17:16:13 +0900

    変数を使用して記述を省略→全ボタンに対してアラートの反映

--- app.js
+++ app.js
@@ -13,14 +13,40 @@ document.getElementById("js-question").textContent = question;
 // for (let i = 0; i < 5; i++) {
 //   document.getElementsByTagName("button")[i].textContent = answers[i];
 // }
-document.getElementsByTagName("button")[0].textContent = answers[0];
-document.getElementsByTagName("button")[1].textContent = answers[1];
-document.getElementsByTagName("button")[2].textContent = answers[2];
-document.getElementsByTagName("button")[3].textContent = answers[3];
+
+const $button = document.getElementsByTagName("button");
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+$button[3].textContent = answers[3];
 
 // ボタンをクリックしたら正誤判定
-document.getElementsByTagName("button")[0].addEventListener("click", () => {
-  if (correct === document.getElementsByTagName("button")[0].textContent) {
+$button[0].addEventListener("click", () => {
+  if (correct === $button[0].textContent) {
+    window.alert("正解!");
+  } else {
+    window.alert("不正解!");
+  }
+});
+
+$button[1].addEventListener("click", () => {
+  if (correct === $button[1].textContent) {
+    window.alert("正解!");
+  } else {
+    window.alert("不正解!");
+  }
+});
+
+$button[2].addEventListener("click", () => {
+  if (correct === $button[2].textContent) {
+    window.alert("正解!");
+  } else {
+    window.alert("不正解!");
+  }
+});
+
+$button[3].addEventListener("click", () => {
+  if (correct === $button[3].textContent) {
     window.alert("正解!");
   } else {
     window.alert("不正解!");

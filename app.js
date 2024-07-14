7b0040aebce07eb1407d2ed3dbcbec7488fe2a69
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-07-14 17:11:13 +0900

    アラートが表示されるようになる

--- app.js
+++ app.js
@@ -8,8 +8,21 @@ const answers = [
 
 const correct = "ニンテンドーDS";
 
+// 定数の文字列をHTMLに反映させる
 document.getElementById("js-question").textContent = question;
+// for (let i = 0; i < 5; i++) {
+//   document.getElementsByTagName("button")[i].textContent = answers[i];
+// }
+document.getElementsByTagName("button")[0].textContent = answers[0];
+document.getElementsByTagName("button")[1].textContent = answers[1];
+document.getElementsByTagName("button")[2].textContent = answers[2];
+document.getElementsByTagName("button")[3].textContent = answers[3];
 
-for (let i = 0; i < 5; i++) {
-  document.getElementsByTagName('button')[i].textContent = answers[i];
-}
+// ボタンをクリックしたら正誤判定
+document.getElementsByTagName("button")[0].addEventListener("click", () => {
+  if (correct === document.getElementsByTagName("button")[0].textContent) {
+    window.alert("正解!");
+  } else {
+    window.alert("不正解!");
+  }
+});

6a867a7b97761ecc54419505927fdf9a81a81f7e
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-07-14 16:46:32 +0900

    質問内容の表示

---
+++ app.js
@@ -0,0 +1,11 @@
+const question = "ゲーム市場、最も売れたゲーム機は次の内どれ？";
+const answers = [
+  "スーパーファミコン",
+  "プレイステーション2",
+  "ニンテンドースイッチ",
+  "ニンテンドーDS",
+];
+
+const correct = "ニンテンドーDS";
+
+document.getElementById("js-question").textContent = question

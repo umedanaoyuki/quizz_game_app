53f74567025420910d073ea751f6a3ca9ffe60d5
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-07-14 17:27:10 +0900

    while文を使ってコードをリファクタリング

--- app.js
+++ app.js
@@ -15,10 +15,18 @@ document.getElementById("js-question").textContent = question;
 // }
 
 const $button = document.getElementsByTagName("button");
-$button[0].textContent = answers[0];
-$button[1].textContent = answers[1];
-$button[2].textContent = answers[2];
-$button[3].textContent = answers[3];
+
+// $button[0].textContent = answers[0];
+// $button[1].textContent = answers[1];
+// $button[2].textContent = answers[2];
+// $button[3].textContent = answers[3];
+
+let buttonIndex = 0;
+let buttonLength = $button.length;
+while (buttonIndex < buttonLength) {
+  $button[buttonIndex].textContent = answers[buttonIndex];
+  buttonIndex++;
+}
 
 // ボタンをクリックしたら正誤判定
 $button[0].addEventListener("click", () => {
